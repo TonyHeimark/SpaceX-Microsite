352c2eac43c1e076b45a4b5f120a1126ecdf0736
Tony Heimark <tjh-94@hotmail.com>
2019-05-13 22:14:55 +0200

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => (
-    <div>
-        Hi from Footer!!
+    <div className="footer">
+        <div className="footer_inner">
+            <div className="footer_logo__container">
+                <img className="logo" src={require("../images/spacex_logo_white.png")} alt="SpaceX logo" />
+            </div>
+            <div className="footer_social__container">
+                <span>Icon 1</span>
+                <span>Icon 2</span>
+                <span>Icon 3</span>
+            </div>
+            <div className="footer_copyright__container">
+                <p>&copy; Copyright 2019 SpaceX all rights reserved.</p>
+            </div>
+        </div>
     </div>
 );
 
