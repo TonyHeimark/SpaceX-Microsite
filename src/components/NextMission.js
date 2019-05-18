b9309ccc5fa53437afa6577030c679d2b17847de
Tony Heimark <tjh-94@hotmail.com>
2019-05-18 18:06:16 +0200

    adding latest changes made from laptop

--- src/components/NextMission.js
+++ src/components/NextMission.js
@@ -28,6 +28,7 @@ export default class App extends React.Component{
             let missionName = data.mission_name;
             let launch = data.launch_date_unix;
             let description = data.details;
+            console.log(launch);
             this.setState(()=>({
                 nextLaunch: [launch],
                 missionName: missionName,
@@ -55,7 +56,7 @@ export default class App extends React.Component{
             <div className="next_mission">
                 <h1 className="next_mission__title">{this.state.missionName}</h1>
                 <span className="next_mission__launch">Launch:</span>
-                <span className="next_mission__countdown">{<Countdown date={launch} />}</span>
+                <span className="next_mission__countdown">{<Countdown date={Date.now() + 86400000} />}</span>
                 <button className="next_mission__button button" onClick={this.handleDescription}>{!this.state.buttonClicked ? "Learn more about this mission" : "Close details"}</button>
                 <div className="next_mission__description">
                     {this.state.buttonClicked ? <p className="next_mission__p">{this.state.description}</p> : <p className="next_mission__p--hidden">{this.state.description}</p>}
