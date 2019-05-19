1bf292319a4fda47468365927bfc4f9669452ef0
Tony Heimark <tjh-94@hotmail.com>
2019-05-19 18:09:32 +0200

    added launch list styling and some route links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Header.css"
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default class App extends React.Component{
 
@@ -35,13 +35,13 @@ export default class App extends React.Component{
         return(
             <header className="header">
                 <div className="header_inner">
-                    <a href="/"><img className="logo" src={require("../images/spacex_logo_white.png")} alt="SpaceX logo" /></a>
+                    <Link to="/"><img className="logo" src={require("../images/spacex_logo_white.png")} alt="SpaceX logo" /></Link>
                 <div onClick={this.handleHamburger} className={ this.state.navStyle === "nav nav-show" ? "change hamburger" : "hamburger"}>
                     <div className="bar1"></div>
                     <div className="bar2"></div>
                     <div className="bar3"></div>
                 </div>
-                <nav className={this.state.navStyle} onClick={window.innerWidth < 1024 ? this.handleHamburger : undefined}>
+                <nav className={this.state.navStyle} onClick={window.innerWidth < 1025 ? this.handleHamburger : undefined}>
                     <NavLink to="/" className="nav_link" activeClassName="is-active" exact={true}>Home</NavLink>
                     <NavLink to="/upcoming-launches" className="nav_link" activeClassName="is-active">Upcoming Launches</NavLink>
                     <NavLink to="/previous-launches" className="nav_link" activeClassName="is-active">Previous Launches</NavLink>
--- src/components/PastMissions.js
+++ src/components/PastMissions.js
@@ -59,40 +59,44 @@ export default class App extends React.Component{
             const launchSite = launch.launch_site.site_name_long;
 
             return (
-                <div key={launchName}>
-                    <div>
-                        <h3>{launchName}</h3>
-                        <p>{launchDate}</p>
-                        <button onClick={this.handleViewDetails}>{this.state.details === launchName ? "Hide Details" : "View Details"}</button>
-                        {launchVideo ? <a target="_blank" rel="noopener noreferrer" href={launchVideo}><button>Watch Video</button></a> : undefined}
-                    </div>
-                    <div className={this.state.details === launchName ? "visible" : "hidden"}>
-                        <span>Details:</span>
-                        <p>{launch.details ? launch.details : "No details to show at this time."}</p>
-                        <span>Rocket:</span>
-                        <p>{launchRocket ? launchRocket : "No machine to show at this time."}</p>
-                        <span>Launch site:</span>
-                        <p>{launchSite ? launchSite : "No launch site to show at this time."}</p>
-                        <span>Payload:</span>
-                        {
-                            launchRocket
-                            ?
+                <div key={launchName} className="launch_box_container">
+                    <div className="launch_box">
+                        <h3 className="launch_title">{launchName}</h3>
+                        <p className="launch_date">{launchDate}</p>
+                        <button className="button launch_button" onClick={this.handleViewDetails}>{this.state.details === launchName ? "Hide Details" : "View Details"}</button>
+                        {launchVideo ? <a target="_blank" rel="noopener noreferrer" href={launchVideo}><button className="button launch_button">Watch Stream</button></a> : undefined}
+                        <div className={this.state.details === launchName ? "visible launch_details_box" : "hidden"}>
                             <div>
-                                {launch.rocket.second_stage.payloads.map((payload) => {
-                                    return (
-                                        <ul key={payload.payload_id}>
-                                            <li>Customer: {payload.customers ? payload.customers : "No info at this time"}</li>
-                                            <li>Type: {payload.payload_type ? payload.payload_type : "No info at this time"}</li>
-                                            <li>Manufacturer: {payload.manufacturer ? payload.manufacturer : "No info at this time"}</li>
-                                            <li>Nationality: {payload.nationality ? payload.nationality : "No info at this time"}</li>
-                                            <li>Orbit: {payload.orbit ? payload.orbit : "No info at this time"}</li>
-                                        </ul>
-                                    )
-                                })}
+                                <span className="launch_details_box__subtitle" >Details:</span>
+                                <p className="launch_details_box__content" >{launch.details ? launch.details : "No details to show at this time."}</p>
+                                <span className="launch_details_box__subtitle" >Rocket:</span>
+                                <p className="launch_details_box__content" >{launchRocket ? launchRocket : "No machine to show at this time."}</p>
+                                <span className="launch_details_box__subtitle" >Launch site:</span>
+                                <p className="launch_details_box__content" >{launchSite ? launchSite : "No launch site to show at this time."}</p>
                             </div>
-                            :
-                            <p>No payloads to show at this time.</p>
-                        }
+                            <div>
+                                <span className="launch_details_box__subtitle" >Payload:</span>
+                                {
+                                    launchRocket
+                                    ?
+                                    <div>
+                                        {launch.rocket.second_stage.payloads.map((payload) => {
+                                            return (
+                                                <ul key={payload.payload_id}>
+                                                    <li className="launch_details_box__payload_item">Customer: {payload.customers ? payload.customers : "No info at this time"}</li>
+                                                    <li className="launch_details_box__payload_item">Type: {payload.payload_type ? payload.payload_type : "No info at this time"}</li>
+                                                    <li className="launch_details_box__payload_item">Manufacturer: {payload.manufacturer ? payload.manufacturer : "No info at this time"}</li>
+                                                    <li className="launch_details_box__payload_item">Nationality: {payload.nationality ? payload.nationality : "No info at this time"}</li>
+                                                    <li className="launch_details_box__payload_item">Orbit: {payload.orbit ? payload.orbit : "No info at this time"}</li>
+                                                </ul>
+                                            )
+                                        })}
+                                    </div>
+                                    :
+                                    <p>No payloads to show at this time.</p>
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
@@ -102,11 +106,13 @@ export default class App extends React.Component{
             <div>
                 {this.state.loading 
                 ?
-                    <CircleSpinner
-                    size={30}
-                    color="#ffffff"
-                    loading={this.state.loading}
-                /> 
+                    <div className="loading_spinner">
+                        <CircleSpinner
+                            size={40}
+                            color="#ffffff"
+                            loading={this.state.loading}
+                        /> 
+                    </div>
                 :
                     jsx
                 }
