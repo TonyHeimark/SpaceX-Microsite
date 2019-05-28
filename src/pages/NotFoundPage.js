8c5d23bb9a566b4b111e1b161a2c68bb99102934
Tony Heimark <tjh-94@hotmail.com>
2019-05-28 16:22:13 +0200

    Added animations

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/NotFoundPage.css";
 
 const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/">Go back to home page</Link>
+    <div className="notfoundpage_container">
+        <h1>Ooops.. Page Not Found</h1>
+        <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <Link to="/"><button className="button">Go back to home page</button></Link>
     </div>
 );
 
